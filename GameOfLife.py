c058afd0c3e1469823c5f5238045fbe5cc61bcb6
Maciek <maciekz1997@gmail.com>
2019-05-22 19:31:46 +0200

    Saving and loading from json, rectangular maps

--- GameOfLife.py
+++ GameOfLife.py
@@ -1,7 +1,47 @@
 import sys
 import random
-from tkinter import *
-from time import sleep
+from tkinter import Tk, Canvas
+import json
+
+
+def calculateNewMap(map):
+    newMap = zeros(len(map[0]), len(map))
+    for x in range(len(newMap)):
+        for y in range(len(newMap[0])):
+            count = countNeighbours(map, x, y)
+            if map[x][y] == 1:
+                if count < 2 or count > 3:
+                    newMap[x][y] = 0
+                else:
+                    newMap[x][y] = 1
+            else:
+                if count == 3:
+                    newMap[x][y] = 1
+    return newMap
+
+
+def saveMap(map, name):
+    f = open(name, "w")
+    data = {
+        'y': len(map),
+        'x': len(map[0]),
+        'cells': []
+    }
+    for x in range(len(map)):
+        for y in range(len(map)):
+            if map[x][y] == 1:
+                data['cells'].append([x, y])
+    f.write(json.dumps(data))
+
+
+def loadMap(filename):
+    f = open(filename)
+    data = json.loads(f.read())
+    cells = data['cells']
+    map = zeros(data['x'], data['y'])
+    for cell in cells:
+        map[cell[0]][cell[1]] = 1
+    return map
 
 
 def generateRandom(x, y):
@@ -20,13 +60,13 @@ def countNeighbours(m, x, y):
         count += m[x + 1][y]
     if y > 0:
         count += m[x][y - 1]
-    if len(m[x]) > y + 1:
+    if len(m[0]) > y + 1:
         count += m[x][y + 1]
     if x > 0 and y > 0:
         count += m[x - 1][y - 1]
-    if x > 0 and len(m[x]) > y + 1:
+    if x > 0 and len(m[0]) > y + 1:
         count += m[x - 1][y + 1]
-    if len(m) > x + 1 and len(m[x]) > y + 1:
+    if len(m) > x + 1 and len(m[0]) > y + 1:
         count += m[x + 1][y + 1]
     if y > 0 and len(m) > x + 1:
         count += m[x + 1][y - 1]
@@ -34,17 +74,18 @@ def countNeighbours(m, x, y):
 
 
 def drawMap(m, w, cellSizeX, cellSizeY):
-
-    for y in range(len(m)):
-        for x in range(len(m[0])):
+    for x in range(len(m)):
+        for y in range(len(m[0])):
             if m[x][y] == 1:
                 f = "#000000"
             else:
                 f = "#ffffff"
             w.create_rectangle(cellSizeX*x, cellSizeY*y, cellSizeX *
-                               x+cellSizeX, cellSizeY*y+cellSizeY, fill=f)
+                               x+cellSizeX, cellSizeY*y+cellSizeY, fill=f,
+                               outline="")
 
 
+# ilość generacji do obliczenia
 n = int(sys.argv[1])
 
 try:
@@ -58,7 +99,7 @@ except IndexError:
     mapHeight = 100
 
 try:
-    map = sys.argv[4]
+    map = loadMap(sys.argv[4])
 except IndexError:
     map = generateRandom(mapWidth, mapHeight)
 
@@ -75,20 +116,8 @@ cellSizeX = canvasWidth/len(map)
 cellSizeY = canvasHeight/len(map[0])
 
 for i in range(n):
-    newMap = zeros(mapWidth, mapHeight)
-    for y in range(len(newMap)):
-        for x in range(len(newMap[0])):
-            count = countNeighbours(map, x, y)
-            if map[x][y] == 1:
-                if count < 2 or count > 3:
-                    newMap[x][y] = 0
-                else:
-                    newMap[x][y] = 1
-            else:
-                if count == 3:
-                    newMap[x][y] = 1
-    map = newMap
+    map = calculateNewMap(map)
     drawMap(map, w, cellSizeX, cellSizeY)
     w.update()
 
-mainloop()
+saveMap(map, "map.json")
